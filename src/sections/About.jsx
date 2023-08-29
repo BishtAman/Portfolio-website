520038a277de7bf85add349bbb5c8210ba5e670c
Aman <uk.amanbisht@gmail.com>
2023-08-29 15:39:13 +0530

    corrected minor styling in home and about pages

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -21,14 +21,14 @@
               alt="profile"
             />
           </section>
-          <section className='w-[100%] '>
+          <section className='w-[100%]'>
             <p className="text-justify text-[22px] leading-[35px] max-lg:leading-[33px] max-lg:text-[18px] max-[400px]:text-[15px] mb-10">
               <span className='text-[35px] max-lg:text-[28px] max-[400px]:text-[20px]'>My name is <span className="text-[#f6bd2e]">Aman Bisht,  </span></span> I'm a self taught developer who started his coding journey on 2021 from learning <span className="text-[#f6bd2e]">HTML</span> because I was interested in building websites that can solve problems.
               <br/> Along with that I also started learning <span className="text-[#f6bd2e]">C</span> language which taught me very essential and deep concepts about programming and it also increased my interest in coding. 
               <br/>Then I learned about <span className="text-[#f6bd2e]">CSS</span> and designing principles and started <span className="text-[#f6bd2e]">JavaScript</span> along with that. I build multiple websites, designs, components of the websites, with the help of CSS and JS frameworks like <span className="text-[#f6bd2e]">Bootstrap</span> and <span className="text-[#f6bd2e]">jQuery</span>.
               <br/> Currently I'm learning <span className="text-[#f6bd2e]">React</span> and <span className="text-[#f6bd2e]">TailwindCSS</span>. I'm also learning <span className="text-[#f6bd2e]">C++</span> concepts to boost my <span className="text-[#f6bd2e]">problem solving</span> skills.
               The sources of my learnings are mostly official <span className="text-[#f6bd2e]">documentation and YouTube</span>.
-              In free time I post my learning on <a href=' ' className="text-[#f6bd2e] hover:text-[#d09500] text-[25px] max-lg:text-[21px] max-[400px]:text-[17px]">Twitter</a> and also write some blogs on Web Development and core programming concepts on <a href=' ' className="text-[#f6bd2e] hover:text-[#d09500] text-[25px] max-lg:text-[21px] max-[400px]:text-[17px]">Hashnode</a> and <a href=' ' className="text-[#f6bd2e]  hover:text-[#d09500] text-[25px] max-lg:text-[21px] max-[400px]:text-[17px]">Medium</a>.
+              In free time I post my learning on <a href='https://twitter.com/@AmanBis68156523' className="text-[#f6bd2e] hover:text-[#d09500] text-[25px] max-lg:text-[21px] max-[400px]:text-[17px]">Twitter</a> and also write some blogs on Web Development and core programming concepts on <a href='https://amanbishtcoder.hashnode.dev/' className="text-[#f6bd2e] hover:text-[#d09500] text-[25px] max-lg:text-[21px] max-[400px]:text-[17px]">Hashnode</a> and <a href='https://medium.com/@amanbisht.dev' className="text-[#f6bd2e]  hover:text-[#d09500] text-[25px] max-lg:text-[21px] max-[400px]:text-[17px]">Medium</a>.
             </p>
             <a
               href={cv}
--- src/sections/Home.jsx
+++ src/sections/Home.jsx
@@ -4,17 +4,17 @@ import frame2 from '../components/assets/frame2.png'
  const Home = (props) => {
   return (
     <section className={` relative  visible text-white poppins h-[88vh] flex flex-col justify-center pl-[20px] ${props.change==0?'visible fadein-transition':'fadeout-transition hidden'}`}>
-        <h1 className='text-[14vh] mx-[175px] max-[928px]:mx-[80px] max-[420px]:mx-[40px] max-lg:text-[10vh] max-[550px]:text-[8vh]'>Hello there<span className='animate-pulse text-[#f6bd2e]'>.</span><span className='animate-pulse text-[#f6bd2e]'>.</span><span className='animate-pulse text-[#f6bd2e]'>.</span> </h1>
-        <h3 className='text-[7vh] mx-[175px] max-[928px]:mx-[80px] max-[420px]:mx-[40px] max-lg:text-[5vh] max-[550px]:text-[3vh]'>I'm <span className='text-[11vh] max-lg:text-[7vh] max-[550px]:text-[5vh] text-[#f6bd2e]'>Aman Bisht,</span></h3>
+        <h1 className='text-[14vh] mx-[175px] max-[928px]:mx-[80px] max-[420px]:mx-[40px] max-lg:text-[10vh] max-[550px]:text-[5vh]'>Hello there<span className='animate-pulse text-[#f6bd2e]'>.</span><span className='animate-pulse text-[#f6bd2e]'>.</span><span className='animate-pulse text-[#f6bd2e]'>.</span> </h1>
+        <h3 className='text-[7vh] mx-[175px] max-[928px]:mx-[80px] max-[420px]:mx-[40px] max-lg:text-[5vh] max-[550px]:text-[2vh]'>I'm  <span className='text-[11vh] max-lg:text-[7vh] max-[550px]:text-[3vh] text-[#f6bd2e]'>Aman Bisht,</span></h3>
         <h3 className='text-[7vh] mx-[175px] max-[928px]:mx-[80px] max-[420px]:mx-[40px] max-lg:text-[5vh] max-[550px]:text-[3vh] max-[440px]:text-[2.5vh] mt-3'>Frontend Web Developer</h3>
         <img
             src={frame1}
-            className='absolute top-[20px] left-[20px] h-[20vh]'
+            className='absolute top-[20px] left-[20px] h-[20vh] max-sm:h-[10vh]'
             alt='frame1'
         />
         <img
             src={frame2}
-            className='absolute bottom-[20px] right-[20px] h-[20vh]'
+            className='absolute bottom-[20px] right-[20px] h-[20vh] max-sm:h-[10vh]'
             alt='frame2'
         />
     </section>
