920546e5b8eafd672656506e7b8620e72d4eda07
Aman <uk.amanbisht@gmail.com>
2023-08-29 18:21:09 +0530

    test

--- src/sections/Blogs.jsx
+++ src/sections/Blogs.jsx
@@ -3,7 +3,7 @@ import images from '../constants/images.js'
 const Blogs = (props) => {
   return (
     <section className={`visible text-white poppins h-[88vh] flex flex-wrap justify-center  ${props.change == 3 ? 'visible fadein-transition' : 'fadeout-transition hidden'}`}>
-      <section className="border-x-4 rounded-lg  max-sm:ml-6 border-[#f6bd2e] px-[2rem] mt-[4rem] max-sm:pl-[1.8rem]">
+      <section className="border-x-4 rounded-lg  max-sm:ml-6 border-[#f6bd2e] px-[2rem] mt-[4rem] max-sm:pl-[1.8rem] max-sm:mt-[2rem]">
           <p className="text-[3rem] max-sm:text-[2rem]">BLOGS</p>
         </section>
       <section className='flex flex-wrap justify-center mt-10'>
